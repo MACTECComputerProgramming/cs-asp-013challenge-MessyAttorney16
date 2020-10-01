1459541c43b1ad507f749ae2f81260aa570b5401
MessyAttorney16 <22ljhalstead@macon.k12.mo.us>
2020-10-01 11:06:33 -0500

    Logan H 4th commit

--- 013 Challenge/MainWindow.xaml.cs
+++ 013 Challenge/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,7 +28,46 @@ namespace _013_Challenge
 
         private void orderButton_Click(object sender, RoutedEventArgs e)
         {
+            choiceLabel.Content = "";
+            costLabel.Content = "";
+            string subsize = "";
+            string bread = "";
+
+            //sub size
+            if (radioSix.IsChecked == true)
+            {
+                subsize = "Six Inch Sub, " ;
+            }
+            else if (radioFoot.IsChecked == true)
+            {
+                subsize = "Foot Long Sub, ";
+            }
             
+            //bread
+            if (radioWheat.IsChecked == true)
+            {
+                bread = "Wheat Bread, ";
+            }
+            else if (radioItalian.IsChecked == true)
+            {
+                bread = "Italian Bread, ";
+            }
+            else if (radioFlat.IsChecked == true)
+            {
+                bread = "Flatbread, ";
+            }
+
+            choiceLabel.Content = subsize + bread;
+
+            ////meat
+            //if (radioHam.IsChecked == true)
+            //{
+            //    choiceLabel.Content = "Ham, ";
+            //}
+            //else if (radioChicken.IsChecked == true)
+            //{
+            //    choiceLabel.Content = "Roasted Chicken, ";
+            //}
         }
     }
 }
