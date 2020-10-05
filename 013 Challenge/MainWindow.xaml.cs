092a79038f96f97a146a6ebbf6b037ce3f1110e0
MessyAttorney16 <22ljhalstead@macon.k12.mo.us>
2020-10-05 10:38:42 -0500

    logan h final commit

--- 013 Challenge/MainWindow.xaml.cs
+++ 013 Challenge/MainWindow.xaml.cs
@@ -30,10 +30,11 @@ namespace _013_Challenge
         {
             choiceLabel.Content = "";
             costLabel.Content = "";
+            
+            //choices
             string subsize = "";
             string bread = "";
             string meat = "";
-            string veggie = "";
             string american = "";
             string swiss = "";
             string pepperjack = "";
@@ -42,15 +43,23 @@ namespace _013_Challenge
             string lettuce = "";
             string onion = "";
             string all = "";
+            
+            //cost
+            double cost = 0;
+            double peppercost = 0;
+            double lettucecost = 0;
+            double onioncost = 0;
 
             //sub size
             if (radioSix.IsChecked == true)
             {
                 subsize = "Six Inch Sub, " ;
+                cost = cost + 3.50;
             }
             else if (radioFoot.IsChecked == true)
             {
                 subsize = "Foot Long Sub, ";
+                cost = cost + 5;
             }
             
             //bread
@@ -65,6 +74,7 @@ namespace _013_Challenge
             else if (radioFlat.IsChecked == true)
             {
                 bread = "Flatbread, ";
+                cost = cost + .50;
             }
 
             //meat
@@ -79,24 +89,29 @@ namespace _013_Challenge
             else if (radioMeatball.IsChecked == true)
             {
                 meat = "Meatball, ";
+                cost = cost + 1;
             }
             else if (radioSteak.IsChecked == true)
             {
                 meat = "Steak, ";
+                cost = cost + 1.50;
             }
 
             //cheese
             if (checkAmerican.IsChecked == true)
             {
                 american = "American, ";
+                cost = cost + .25;
             }
             if (checkSwiss.IsChecked == true)
             {
                 swiss = "Swiss, ";
+                cost = cost + .30;
             }
             if (checkPepperjack.IsChecked == true)
             {
                 pepperjack = "Pepperjack, ";
+                cost = cost + .75;
             }
 
             //veggie
@@ -107,14 +122,17 @@ namespace _013_Challenge
             if (checkPepper.IsChecked == true)
             {
                 pepper = "Green Pepper, ";
+                peppercost = .25;
             }
             if (checkLettuce.IsChecked == true)
             {
                 lettuce = "Lettuce, ";
+                lettucecost = .15;
             }
             if (checkOnion.IsChecked == true)
             {
                 onion = "Red Onion, ";
+                onioncost = .10;
             }
             if (checkAll.IsChecked == true)
             {
@@ -123,10 +141,16 @@ namespace _013_Challenge
                 pepper = "";
                 lettuce = "";
                 onion = "";
+
+                cost = cost + .25 - peppercost - lettucecost - onioncost;
             }
-
+            if ((radioFoot.IsChecked == true)&&(radioWheat.IsChecked == true)&&(radioMeatball.IsChecked == true)&&(checkAmerican.IsChecked == true))
+            {
+                cost = cost - 2.00;
+            }
+            cost = cost + peppercost + lettucecost + onioncost;
             choiceLabel.Content = subsize + bread + meat + american + swiss + pepperjack + cucumber + pepper + lettuce + onion + all;
-
+            costLabel.Content = "$" + cost;
 
         }
     }
